2b1aa41ce07304cc130dd93cfa258691e167a77c
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-15 18:38:36 -0300

    Created Page Home

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,10 +1,10 @@
-import {ReactComponent as Icon} from '../../assets/logo.svg';
+import {ReactComponent as Logo} from '../../assets/logo.svg';
 import './styles.css';
 
 const Header = () => (
 
      <header className="main-header">
-         <Icon />
+         <Logo />
          <div className="logo-text">
            <span className="logo-text-1">Big Game</span>
            <span className="logo-text-2"> Survey</span>
--- front-web/src/components/pages/home/index.tsx
+++ front-web/src/components/pages/home/index.tsx
@@ -1,12 +1,23 @@
-const Home = () => {
-    return (
+import {ReactComponent as ArrowIcon} from '../../../assets/arrow.svg';
+import {ReactComponent as GamerImage} from '../../../assets/gamer.svg';
+import './styles.css';
+
+const Home = () => (
       <div className="home-container">
           <div className="home-text">
-            <p className="home-text-title">Quais jogos a galera gosta mais?</p>
-            <p className="home-text-subtitle">Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo</p>
+            <h1 className="home-text-title">Quais jogos a galera gosta mais?</h1>
+            <h3 className="home-text-subtitle">Clique no botão abaixo e saiba quais são os jogos que os gamers estão escolhendo!</h3>
+            <div className="home-actions">
+               <button className="home-btn">
+                 QUERO SABER QUAIS SÃO
+               </button>
+               <div className="home-btn-icon">
+                 <ArrowIcon />
+               </div>
+            </div>
           </div>
+          <GamerImage />
       </div>
-    )
-}
+); 
 
 export default Home;
