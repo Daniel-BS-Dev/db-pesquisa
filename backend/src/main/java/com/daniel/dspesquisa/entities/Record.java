09b1919abaf638798a0c0db23de2ebf55f0c1df1
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-15 12:42:04 -0300

    Entities Mapping JPA

--- backend/src/main/java/com/daniel/dspesquisa/entities/Record.java
+++ backend/src/main/java/com/daniel/dspesquisa/entities/Record.java
@@ -3,16 +3,31 @@ package com.daniel.dspesquisa.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_record")
 public class Record implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Integer age;
 	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
 	
+	@ManyToOne
+	@JoinColumn(name="game_id")
 	private Game game;
 
 	public Record() {
