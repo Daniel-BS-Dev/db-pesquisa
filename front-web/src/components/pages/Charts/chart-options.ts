8df927c6587746d4fc1e6e922f8b69d9aa3b3efa
Daniel-BS-Dev <danielbenedito355@gmail.com>
2021-12-16 17:09:58 -0300

    Adding page of charts

--- front-web/src/components/pages/Charts/chart-options.ts
+++ front-web/src/components/pages/Charts/chart-options.ts
@@ -1,36 +1,3 @@
-export const pieOptions = {
-  chart: {
-    background: "transparent",
-    foreColor: "red",
-  },
-  colors: ["#a8dadc", "#ed7947", "#00D4FF", "#ffd6a5"],
-  legend: {
-    show: false,
-  },
-  tooltip: {
-    enabled: true,
-  },
-  dataLabels: {
-    enabled: true,
-    offsetX: "100px",
-    offsetY: "100px",
-    style: {
-      colors: ["#a8dadc", "#ed7947", "#00D4FF", "#ffd6a5"],
-      fontSize: "20px",
-      fontFamily: "Play, sans-serif",
-      fontWeight: 700,
-    },
-  },
-  plotOptions: {
-    pie: {
-      customScale: 0.7,
-      expandOnClick: false,
-      dataLabels: {
-        offset: 60,
-      },
-    },
-  },
-};
 
 export const barOptions = {
   chart: {
--- front-web/src/components/pages/Charts/index.tsx
+++ front-web/src/components/pages/Charts/index.tsx
@@ -1,20 +1,56 @@
 // para usar os graficos eu importei a biblioteca apexcharts
 // não posso esquecer de usar a dependencia do apexcharts o apexcharts
+// documentação https://apexcharts.com/docs/react-charts/
+import { buildBarSeries, getGenderChartData, getPlatformChartData } from "./helpers";
 import Filter from '../../../components/Filter';
-import { barOptions } from './chart-options'
+import { useState, useEffect } from 'react';
+import { barOptions} from './chart-options';
 import Chart from 'react-apexcharts';
+import axios from 'axios';
 import './styles.css';
 
-
-
-const chartData = [
-    {
-        x: 'daniel',
-        y:10
+type ChartData = {
+        labels: string[],
+        series: number[]
     }
-]
+
+type BarChartData = {
+        x: string,
+        y: number
+    }
+
+const initialChartData = {
+    labels: [],
+    series: []
+}
+
+const BASE_URL = 'https://dspesquisa-daniel.herokuapp.com';
 
 const Charts = () => {
+  
+    const [barChartData, setbarChartData] = useState<BarChartData[]>([]);
+    const [platformData, setPlatformData] = useState<ChartData>(initialChartData);
+    const [genderData, setGenderData] = useState<ChartData>(initialChartData);
+
+   useEffect(() => {
+      async function getData(){
+         const recordsResponse = await axios.get(`${BASE_URL}/records`);
+         const gamesResponse = await axios.get(`${BASE_URL}/games`);
+
+          const barData = buildBarSeries(gamesResponse.data, recordsResponse.data.content);
+          setbarChartData(barData);
+
+          const platformChartData = getPlatformChartData(recordsResponse.data.content);
+          setPlatformData(platformChartData);
+
+          const genderChartData = getGenderChartData(recordsResponse.data.content);
+          setGenderData(genderChartData);
+     
+         
+       }
+       getData();
+   }, []);
+    
     return(
          <div className="page-container">
            <Filter link="/record" linkText="VER TABELA" />
@@ -24,23 +60,37 @@ const Charts = () => {
                    Jogos mais votados
                  </h1>
                  <div className="games-container">
-                   <Chart 
+                  <Chart 
                      options={barOptions}
                      type="bar"
-                     width="900"
-                     height='500'
-                     series={[{data: chartData}]}
-
+                     width="600"
+                     height='650'
+                     series={[{data: barChartData}]}
                    />
                  </div>
               </div>
+           <div className="charts">
+             <div className="platform-chart">
+               <h2 className="chart-title">Plataformas</h2>
+               <Chart
+                   options={{...barOptions, labels: platformData?.labels}}
+                   type="bar"
+                   width="350"
+                   series={[{data: platformData?.series}]}
+                />
+             </div>
+             <div className="gender-chart">
+               <h2 className="chart-title">Gêneros</h2>
+               <Chart
+                   options={{...barOptions, labels: genderData?.labels}}
+                   type="bar"
+                   width="350"
+                   series={[{data: genderData?.series}]}
+                />
+             </div>
            </div>
-         
-         
-         
-         
          </div>
-        
+        </div>
     );
 }
 
